8c82a536cb58fb09a94b2ff2cde592413e80ebd0
gavinganza <g.ganza@alustudent.com>
2025-10-27 13:28:24 +0000

    swish

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    """Retrieve an element from a list safely like in C"""
-    if idx < 0 or idx >= len(my_list):
+    listlen = len(my_list)
+    if idx < 0 or idx >= listlen:
         return None
     return my_list[idx]
-
