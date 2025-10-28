f0d2a5251faafa75ce49649989119c658679df0c
gavinganza <g.ganza@alustudent.com>
2025-10-28 15:43:43 +0000

    meme culture

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maximum = my_list[0]
     if len(my_list) == 0:
         return None
     else:
+        maximum = my_list[0]
         for i in my_list:
             if i > maximum:
                 maximum = i
